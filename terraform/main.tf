9f9e6fdb6362b7467823c5a19442dd50bd812a40
Pratikraut@12345 <pratikraut88895@gmail.com>
2024-01-13 18:36:28 +0530

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,41 +17,41 @@ resource "github_actions_workflow" "deploy" {
 
   resolves = ["Deploy"]
 
-  workflow_content = <<EOF
-name: Deploy
+  workflow_content = <<-EOF
+    name: Deploy
 
-on:
-  push:
-    branches:
-      - master
+    on:
+      push:
+        branches:
+          - main
 
+    jobs:
+      build:
+        runs-on: ubuntu-latest
 
-jobs:
-  build:
-    runs-on: ubuntu-latest
+        steps:
+          - name: Checkout Repository
+            uses: actions/checkout@v2
 
-    steps:
-    - name: Checkout Repository
-      uses: actions/checkout@v2
+          - name: Set up Node.js
+            uses: actions/setup-node@v3
+            with:
+              node-version: 14
 
-    - name: Set up Node.js
-      uses: actions/setup-node@v3
-      with:
-        node-version: 14
+          - name: Install Dependencies
+            run: npm install
 
-    - name: Install Dependencies
-      run: npm install
+          - name: Build Angular App
+            run: npm run build --prod
 
-    - name: Build Angular App
-      run: npm run build --prod
-
-    - name: Deploy to GitHub Pages
-      run: |
-        git config --global user.email "actions@github.com"
-        git config --global user.name "GitHub Actions"
-        npx angular-cli-ghpages --dir=dist/todo
-EOF
+          - name: Deploy to GitHub Pages
+            run: |
+              git config --global user.email "actions@github.com"
+              git config --global user.name "GitHub Actions"
+              npx angular-cli-ghpages --dir=dist/angular-app
+  EOF
 }
+
 
 output "github_pages_url" {
   value = "https://${github_repository.todo.full_name}.github.io/"
